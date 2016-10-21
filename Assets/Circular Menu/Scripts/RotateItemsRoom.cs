e8b1224a0e9699825da594525b3c40d016bd07e2
Danning Lu <shgfl@hotmail.com>
2016-10-21 17:09:19 -0400

    update

---
+++ Assets/Circular Menu/Scripts/RotateItemsRoom.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateItemsRoom :VRObject {
+
+	bool canControlRotate = false;
+	Vector3 initMousePos;
+	private float  startAngle = 0;
+	private float rotX, rotY;
+	private Vector3 origRot;
+	Vector3 lastPos;
+	public Transform ec;
+	public GameObject camPos;
+
+	bool checkForUI = false;
+
+	float timerToCheck = 0.5f;
+
+	float timeLeft = 0.5f;
+
+	public float xAngle;
+
+	Vector3 previousPos;
+	bool didNotMove = false;
+
+	Cursor curCursor;
+	public bool mUpdate = false;
+
+	// Use this for initialization
+	void Start () {
+		origRot = this.transform.eulerAngles;
+		rotX = origRot.x;
+		rotY = origRot.y;
+		ec = GameObject.Find ("CamLocks").transform;
+	}
+
+
+
+	Vector3 WorldSpaceCords () {
+
+		//this was to keep units same for draw + menu detection
+		Vector3 cursorCoord = LeftCursor.instance.transform.position;
+		Vector3 headCoord = Camera.main.transform.position;
+		Vector3 vec = cursorCoord - headCoord;
+		float radius = 30;
+		if (radius > headCoord.y)
+			radius = headCoord.y;
+		Vector3 finalCoord = headCoord + vec.normalized * radius;
+		return finalCoord;
+	}
+
+
+	public override void start (ref Cursor c) {
+
+		BlockUI.instance.TurnOff ();
+
+
+		rotX = ec.transform.eulerAngles.x;
+		rotY = ec.transform.eulerAngles.y;
+
+
+		initMousePos = Camera.main.WorldToScreenPoint(c.transform.position);
+		mUpdate = true;
+		curCursor = c;
+		if (c == RightCursor.instance && !UIManage.instance.isDraw ) {
+			canControlRotate = true;
+		} else if (c== LeftCursor.instance && !UIManage.instance.isDraw ) {
+			if (!UIManage.instance.isOn) {
+				checkForUI = true;
+			} else {
+				checkForUI = false;
+			}
+		}
+	}
+
+	public override void end() {
+		canControlRotate = false;
+		mUpdate = false;
+		checkForUI = false;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if(LeftCursor.instance.clicked && RightCursor.instance.clicked){
+			canControlRotate = false;
+			checkForUI = false;
+			UIManage.instance.turnOffUI();
+
+		}
+
+
+		if (canControlRotate) {
+
+			Vector3 curCursorPos = Camera.main.WorldToScreenPoint (curCursor.transform.position);
+
+			float deltaX = initMousePos.x - curCursorPos.x;
+			float deltaY = initMousePos.y - curCursorPos.y;
+			rotX -= deltaY * Time.deltaTime  * 2 * -1;
+			rotY += deltaX * Time.deltaTime  * 2 * -1 ;
+			ec.transform.eulerAngles = new Vector3 (rotX, rotY, 0);
+			initMousePos =  Camera.main.WorldToScreenPoint (curCursor.transform.position);
+
+		}
+
+
+		if(checkForUI) {
+
+			timeLeft -= Time.deltaTime;
+			UIManage.instance.RingActivation (LeftCursor.instance.transform.localPosition, true);
+
+			if (timeLeft <= timerToCheck) {
+
+				checkForUI = false;
+				timeLeft = timerToCheck;
+				UIManage.instance.turnOnUI (LeftCursor.instance);
+
+			}
+
+
+		}
+
+
+
+	}
+}
---
+++ Assets/DL_Test_Scenes/Script/RaycastDistance.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastDistance : MonoBehaviour {
+
+	public GameObject avatar;
+	public float distance = 20f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//RayCast.instance.StopZooming = true;
+		//print("this object's transform direction is " + avatar.transform.TransformDirection(avatar.transform.position));
+		Vector3 direction = avatar.transform.TransformDirection (avatar.transform.position);
+		RaycastHit hit;
+		Vector3 tempdirection = direction;
+		tempdirection.x = -tempdirection.x; tempdirection.y = -tempdirection.y;
+		tempdirection.z = -tempdirection.z;
+		if (Physics.Raycast (avatar.transform.position, tempdirection, out hit, distance)) {
+			//if(hit.collider.gameObject.transform.parent.name)
+			print ("hit " + hit.collider.gameObject.transform.parent.name);
+			RayCast.instance.StopZooming = true;
+		}
+	
+	}
+}
